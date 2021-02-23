7b9d4a84b56d9c92786cf30c956d2482f318754b
itachi <kr9_hire@mail.ru>
2021-02-23 22:03:13 +0300

    Completed

---
+++ lab2_task1/lab2_task1/lab2_task1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+//Reverse words
+namespace lab2_task1
+{
+    class lab2_task1
+    {
+        static void Main(string[] args)
+        {
+          Console
+                 .ReadLine()
+                 .Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                 .Reverse()
+                 .ToList()
+                 .ForEach(item => Console.Write(item + " "));
+        }
+    }
+}
