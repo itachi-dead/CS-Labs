4e7f4e89e3d6297b79d601592e7c133149202f67
itachi <kr9_hire@mail.ru>
2021-03-15 06:14:03 +0300

    lab3 fix counter

---
+++ lab3/lab3/ALU.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace lab3
+{
+    class ALU
+    {
+        public static int counter = 0;
+        public ALU()
+        {
+            counter++;
+            Id = counter;
+        }
+        public ALU(string name)
+        {
+
+        }
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Brand
+        {
+            get;
+            set;
+        }
+
+        public void Print()
+        {     
+                Console.WriteLine(Id);
+                Console.WriteLine($"\t{Name}");
+                Console.WriteLine($"\t{Brand}");
+        }
+        public void Print(string IdName)
+        {
+            Console.WriteLine($"{Id} - {Name}");
+        }
+
+        public string FindAndRemove(string Id, List<ALU>a)
+        {
+            bool found = false;
+            foreach(ALU i in a)
+            {
+                    if (i.Id.ToString().Equals(Id))
+                    {
+                        a.Remove(i);
+                        found = true;
+                        Change(i, a);
+                        break;
+                    }
+            }
+            return found? "Succesfully removed" :"there is no such an Id";
+        }
+        public void Change(ALU i ,List<ALU>a)
+        {
+            for (int j = i.Id - 1; j < a.Count; j++)
+            {
+                a[j].Id--;
+            }
+        }
+    }
+}
---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool stop = false;
+            List<ALU> proccsesors = new List<ALU>();
+            string n = "";
+            while (!stop)
+            {
+                Console.WriteLine("What do u want to do:");
+                Console.WriteLine("Enter 1 to add proccesor");
+                Console.WriteLine("Enter 2 to show all information");
+                Console.WriteLine("Enter 3 to delete proccesor by Id:");
+                Console.WriteLine("Enter 0 to end");
+
+                n = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (n) {
+                    case "1":
+                        ALU temp = new ALU();
+
+                        Console.Write("Enter name of proccesor:");
+                        temp.Name = Console.ReadLine();
+
+                        Console.Write("Enter brand:");
+                        temp.Brand = Console.ReadLine();
+
+                        proccsesors.Add(temp);
+                        break;
+                    case "2":
+                        foreach(ALU i in proccsesors)
+                        {
+                            i.Print();
+                        }
+                        break;
+                    case "3":
+                        foreach (ALU i in proccsesors)
+                        {
+                            i.Print("Id and Name");
+                        }
+                        Console.WriteLine("Enter id to delete");
+                        string Id = "";
+                        Id = Console.ReadLine();
+                        ALU searching = new ALU("no counter");
+                        if(searching.FindAndRemove(Id , proccsesors).Equals("Succesfully removed"))
+                        {
+                            Console.WriteLine("Succesfully removed");
+                        }
+                        else
+                        {
+                            Console.WriteLine("There is no such and Id");
+                        }
+                        break;
+                    case "0":
+                        stop = true;
+                        break;
+                }
+                Console.WriteLine();
+
+               
+            }
+        }
+    }
+}
