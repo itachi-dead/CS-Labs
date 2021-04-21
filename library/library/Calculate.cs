78928567e94541df24fa4925b8ccc456e92b20c6
itachi <kr9_hire@mail.ru>
2021-04-22 02:25:10 +0400

    completed

---
+++ library/library/Calculate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace library
+{
+    public class Calculate
+    {
+        public int Addition(int a , int b)
+        {
+            return a + b;
+        }
+
+        public int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public double Sinus(double a)
+        {
+            return Math.Sin(a);
+        }
+
+        public double Cosinus(double a)
+        {
+            return Math.Cos(a);
+        }
+
+        public double Reverse(double a)
+        {
+            return 1 / a;
+        }
+
+        public double Exponenta(double a)
+        {
+            return Math.Exp(a);
+        }
+    }
+}
